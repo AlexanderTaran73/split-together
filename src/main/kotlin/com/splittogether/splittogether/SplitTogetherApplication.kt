be288cda9c644d21d713fa70d36b71f455ccfcae
Alexander Taran <ataran@ultimeta.ru>
2026-05-27 00:17:23 +0300

    Initial commit

---
+++ src/main/kotlin/com/splittogether/splittogether/SplitTogetherApplication.kt
@@ -0,0 +1,11 @@
+package com.splittogether.splittogether
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitTogetherApplication
+
+fun main(args: Array<String>) {
+	runApplication<SplitTogetherApplication>(*args)
+}
---
+++ src/test/kotlin/com/splittogether/splittogether/SplitTogetherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.splittogether.splittogether
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplitTogetherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
